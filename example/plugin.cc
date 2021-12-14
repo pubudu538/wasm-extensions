f33723aa987bdb1b4370e74c6b360a3dd70c45fa
pubudu538 <pubudu538@gmail.com>
2021-12-14 14:24:53 +0000

    dd

--- example/plugin.cc
+++ example/plugin.cc
@@ -7,6 +7,6 @@ static RegisterContextFactory register_Example(CONTEXT_FACTORY(PluginContext),
 bool PluginRootContext::onConfigure(size_t) { return true; }
 
 FilterHeadersStatus PluginContext::onResponseHeaders(uint32_t, bool) {
-  addResponseHeader("X-Wasm-custom", "foo");
+  addResponseHeader("X-dWasm-custom", "foo");
   return FilterHeadersStatus::Continue;
 }
