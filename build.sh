435c1e8ae4ff376bda56ea3178c5da79b161e26f
pubudu538 <pubudu538@gmail.com>
2021-12-16 09:15:25 +0000

    Thu Dec 16 09:15:25 UTC 2021

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+git pull --rebase origin master
 cd example/
 bazel build //:example.wasm
 cp /home/pubududemo/.cache/bazel/_bazel_pubududemo/0801b2896c776c9311f86b69c49ba8f4/execroot/example_extension/bazel-out/k8-fastbuild/bin/example.wasm ./
