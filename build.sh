89fc68a6696acb2a1fdf680b3a235155c39dcc49
pubudu538 <pubudu538@gmail.com>
2021-12-16 08:54:50 +0000

    Thu Dec 16 08:54:50 UTC 2021

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd example/
 bazel build //:example.wasm
-cp bazel-bin/example.wasm ./
+cp /home/pubududemo/.cache/bazel/_bazel_pubududemo/0801b2896c776c9311f86b69c49ba8f4/execroot/example_extension/bazel-out/k8-fastbuild/bin/example.wasm ./
 cd ../
 date_value=$(date)
 git commit -a -m "$date_value"
