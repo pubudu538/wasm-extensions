df66b04541342a4b43f71c1ccd59c0be7bc5f4db
pubudu538 <pubudu538@gmail.com>
2021-12-21 14:20:57 +0530

    Tue Dec 21 14:20:57 +0530 2021

--- extensions/wso2_istio/istio_wasm.cc
+++ extensions/wso2_istio/istio_wasm.cc
@@ -2,8 +2,8 @@
 #include <unordered_map>
 
 // #include "absl/strings/str_cat.h"
-#include "proxy_wasm_intrinsics.h"
 #include "extensions/common/wasm/json_util.h"
+#include "proxy_wasm_intrinsics.h"
 
 using ::nlohmann::json;
 using ::Wasm::Common::JsonArrayIterate;
@@ -12,24 +12,27 @@ using ::Wasm::Common::JsonObjectIterate;
 using ::Wasm::Common::JsonValueAs;
 
 class ExampleRootContext : public RootContext {
-public:
-  explicit ExampleRootContext(uint32_t id, std::string_view root_id) : RootContext(id, root_id) {}
+ public:
+  explicit ExampleRootContext(uint32_t id, std::string_view root_id)
+      : RootContext(id, root_id) {}
 
   bool onStart(size_t) override;
   bool onConfigure(size_t) override;
 };
 
 class ExampleContext : public Context {
-public:
+ public:
   explicit ExampleContext(uint32_t id, RootContext *root) : Context(id, root) {}
 
-  FilterHeadersStatus onRequestHeaders(uint32_t headers, bool end_of_stream) override;
+  FilterHeadersStatus onRequestHeaders(uint32_t headers,
+                                       bool end_of_stream) override;
   void onDone() override;
 };
-static RegisterContextFactory register_ExampleContext(CONTEXT_FACTORY(ExampleContext),
-                                                      ROOT_FACTORY(ExampleRootContext));
+static RegisterContextFactory register_ExampleContext(
+    CONTEXT_FACTORY(ExampleContext), ROOT_FACTORY(ExampleRootContext));
 
-FilterHeadersStatus ExampleContext::onRequestHeaders(uint32_t headers, bool end_of_stream) {
+FilterHeadersStatus ExampleContext::onRequestHeaders(uint32_t headers,
+                                                     bool end_of_stream) {
   logInfo(std::string("onRequdestsHeaders ") + std::to_string(id()));
   auto path = getRequestHeader(":path");
   logInfo(std::string("header path ") + std::string(path->view()));
@@ -55,21 +58,22 @@ bool ExampleRootContext::onConfigure(size_t config_size) {
   logInfo("onConfigure");
   // proxy_set_tick_period_milliseconds(1000); // 1 sec
 
-  auto configuration_data = getBufferBytes(WasmBufferType::PluginConfiguration, 0, config_size);
+  auto configuration_data =
+      getBufferBytes(WasmBufferType::PluginConfiguration, 0, config_size);
 
   logInfo("onConfigure.......");
   logInfo(configuration_data->view());
 
-
   // Parse configuration JSON string.
   auto result = ::Wasm::Common::JsonParse(configuration_data->view());
   if (!result.has_value()) {
+    LOG_WARN(absl::StrCat("cannot parse plugin configuration JSON string: ",
+                          configuration_data->view()));
     logWarn("cannot parse plugin configuration JSON string: ");
     return false;
   } else {
     logInfo("onConfigure....... - Parsing done@@@!!");
   }
-
 
   return true;
 }
