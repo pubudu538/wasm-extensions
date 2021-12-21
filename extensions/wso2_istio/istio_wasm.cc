55f05d0d81d34b0aaf91611bf5701c9bcf3c876c
pubudu538 <pubudu538@gmail.com>
2021-12-21 15:01:01 +0530

    Tue Dec 21 15:01:01 +0530 2021

--- extensions/wso2_istio/istio_wasm.cc
+++ extensions/wso2_istio/istio_wasm.cc
@@ -40,7 +40,7 @@ FilterHeadersStatus ExampleContext::onRequestHeaders(uint32_t headers,
 
   logInfo("opa_host_->view()");
 
-  logInfo(opa_host_->view());
+  // logInfo(opa_host_->view());
   return FilterHeadersStatus::Continue;
 }
 
@@ -95,7 +95,7 @@ bool ExampleRootContext::onConfigure(size_t config_size) {
     //   return false;
     // }
     opa_host_ = opa_host_val.first.value();
-    logInfo(opa_host_);
+    // logInfo(opa_host_);
   } else {
     LOG_WARN(
         absl::StrCat("opa service host must be provided in plugin "
