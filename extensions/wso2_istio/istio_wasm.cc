f5d05d4cc687e751fb07ec64f776027909df5a0f
pubudu538 <pubudu538@gmail.com>
2021-12-21 20:26:27 +0530

    Tue Dec 21 20:26:27 +0530 2021

--- extensions/wso2_istio/istio_wasm.cc
+++ extensions/wso2_istio/istio_wasm.cc
@@ -7,10 +7,23 @@
 #include "extensions/common/wasm/json_util.h"
 #include "proxy_wasm_intrinsics.h"
 
-//using ::nlohmann::json;
-//using ::Wasm::Common::JsonArrayIterate;
-//using ::Wasm::Common::JsonGetField;
-//using ::Wasm::Common::JsonObjectIterate;
+#include "proxy_wasm_intrinsics_lite.pb.h"
+#include "google/protobuf/util/json_util.h"
+
+
+#include "extensions/wso2_istio/echo/echo.pb.h"
+
+static constexpr char EchoServerServiceName[] = "echo.EchoServer";
+static constexpr char SayHelloMethodName[] = "SayHello";
+
+//using google::protobuf::util::JsonParseOptions;
+////using google::protobuf::util::error::Code;
+//using google::protobuf::util::Status;
+
+using echo::EchoRequest;
+using echo::EchoReply;
+using echo::Config;
+
 using ::Wasm::Common::JsonValueAs;
 
 static RegisterContextFactory register_ExampleContext(
@@ -41,35 +54,35 @@ FilterHeadersStatus ExampleRootContext::check() {
   logInfo(opa_host_);
   return FilterHeadersStatus::Continue;
 
-  // std::string jwt_string;
-  // if (!getValue(
-  //         {"metadata", "filter_metadata", "envoy.filters.http.jwt_authn",
-  //         "my_payload", "sub"}, &jwt_string)) {
-  //   LOG_ERROR(std::string("filter_metadata Error ") + std::to_string(id()));
-  // }
-
-  // LOG_INFO(">>>>>>>>>>>>>  Calling GRPC for sub:" + jwt_string);
-  // ExampleRootContext *a = dynamic_cast<ExampleRootContext*>(root());
-  // GrpcService grpc_service;
-  // grpc_service.mutable_envoy_grpc()->set_cluster_name(a->opa_host_);
-  // std::string grpc_service_string;
-  // grpc_service.SerializeToString(&grpc_service_string);
-
-  // EchoRequest request;
-  // request.set_name(jwt_string);
-  // std::string st2r = request.SerializeAsString();
-  // HeaderStringPairs initial_metadata;
-  // initial_metadata.push_back(std::pair("parent", "bar"));
-  // auto res =  root()->grpcCallHandler(grpc_service_string,
-  // EchoServerServiceName, SayHelloMethodName, initial_metadata, st2r, 1000,
-  //                             std::unique_ptr<GrpcCallHandlerBase>(new
-  //                             MyGrpcCallHandler(this)));
-
-  // if (res != WasmResult::Ok) {
-  //   LOG_ERROR("Calling gRPC server failed: " + toString(res));
-  // }
-
-  // return FilterHeadersStatus::StopIteration;
+//   std::string jwt_string;
+//   if (!getValue(
+//           {"metadata", "filter_metadata", "envoy.filters.http.jwt_authn",
+//           "my_payload", "sub"}, &jwt_string)) {
+//     LOG_ERROR(std::string("filter_metadata Error ") + std::to_string(id()));
+//   }
+//
+//   LOG_INFO(">>>>>>>>>>>>>  Calling GRPC for sub:" + jwt_string);
+//   ExampleRootContext *a = dynamic_cast<ExampleRootContext*>(rootContext());
+//   GrpcService grpc_service;
+//   grpc_service.mutable_envoy_grpc()->set_cluster_name(a->opa_host_);
+//   std::string grpc_service_string;
+//   grpc_service.SerializeToString(&grpc_service_string);
+//
+//   EchoRequest request;
+//   request.set_name(jwt_string);
+//   std::string st2r = request.SerializeAsString();
+//   HeaderStringPairs initial_metadata;
+//   initial_metadata.push_back(std::pair("parent", "bar"));
+//   auto res =  rootContext()->grpcCallHandler(grpc_service_string,
+//   EchoServerServiceName, SayHelloMethodName, initial_metadata, st2r, 1000,
+//                               std::unique_ptr<GrpcCallHandlerBase>(new
+//                               MyGrpcCallHandler(this)));
+//
+//   if (res != WasmResult::Ok) {
+//     LOG_ERROR("Calling gRPC server failed: " + toString(res));
+//   }
+//
+//   return FilterHeadersStatus::StopIteration;
 }
 
 bool ExampleRootContext::onConfigure(size_t config_size) {
@@ -108,37 +121,36 @@ bool ExampleRootContext::onConfigure(size_t config_size) {
   return true;
 }
 
-// class MyGrpcCallHandler : public GrpcCallHandler<google::protobuf::Value> {
-//  public:
-//   MyGrpcCallHandler(ExampleContext *context) { context_ = context;  }
+class MyGrpcCallHandler : public GrpcCallHandler<google::protobuf::Value> {
+  public:
+   MyGrpcCallHandler(ExampleContext *context) { context_ = context;  }
 
-//   void onSuccess(size_t body_size) override {
-//     LOG_INFO("GRPC call SUCCESS");
-//     WasmDataPtr response_data =
-//     getBufferBytes(WasmBufferType::GrpcReceiveBuffer, 0, body_size); const
-//     EchoReply& response = response_data->proto<EchoReply>(); LOG_INFO("got
-//     gRPC Response: " + response.message());
+   void onSuccess(size_t body_size) override {
+     LOG_INFO("GRPC call SUCCESS");
+     WasmDataPtr response_data =
+     getBufferBytes(WasmBufferType::GrpcReceiveBuffer, 0, body_size); const
+     EchoReply& response = response_data->proto<EchoReply>(); LOG_INFO("got gRPC Response: " + response.message());
 
-//     context_->setEffectiveContext();
+     context_->setEffectiveContext();
 
-//     auto res = addRequestHeader("isAdmin", response.message());
-//     if (res != WasmResult::Ok) {
-//       LOG_ERROR("Modifying Header data failed: " + toString(res));
-//     }
-//     continueRequest();
-//   }
+     auto res = addRequestHeader("isAdmin", response.message());
+     if (res != WasmResult::Ok) {
+       LOG_ERROR("Modifying Header data failed: " + toString(res));
+     }
+     continueRequest();
+   }
 
-//   void onFailure(GrpcStatus status) override {
-//     LOG_INFO(" GRPC call FAILURE ");
-//     auto p = getStatus();
-//     LOG_DEBUG(std::string("failure ") +
-//     std::to_string(static_cast<int>(status)) +
-//              std::string(p.second->view()));
-//     context_->setEffectiveContext();
-//     closeRequest();
-//   }
+   void onFailure(GrpcStatus status) override {
+     LOG_INFO(" GRPC call FAILURE ");
+     auto p = getStatus();
+     LOG_DEBUG(std::string("failure ") +
+     std::to_string(static_cast<int>(status)) +
+              std::string(p.second->view()));
+     context_->setEffectiveContext();
+     closeRequest();
+   }
 
-//  private:
-//   ExampleContext *context_;
+  private:
+   ExampleContext *context_;
 
-// };
+ };
